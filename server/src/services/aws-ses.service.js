a40b690c6bc672d04d956280c99c9ec004174648
Nikhil shinde <nikhilsubsun123@gmail.com>
2026-07-29 19:34:16 +0530

    Revert to billing@classgrid.in as requested

--- server/src/services/aws-ses.service.js
+++ server/src/services/aws-ses.service.js
@@ -27,8 +27,11 @@ transporter.verify((err) => {
 export const sendEmail = async ({ to, subject, html, text, fromName, fromEmail, replyTo }) => {
   console.log(`[AWS SES] Sending to: ${to} | From: ${fromEmail}`);
 
+  const defaultFromName = process.env.AWS_SES_DEFAULT_FROM_NAME || "Classgrid Platform";
+  const defaultFromEmail = process.env.AWS_SES_DEFAULT_FROM_EMAIL || "support@classgrid.in";
+
   const info = await transporter.sendMail({
-    from: `"${fromName}" <${fromEmail}>`,
+    from: `"${fromName || defaultFromName}" <${fromEmail || defaultFromEmail}>`,
     replyTo: replyTo || "support@classgrid.in",
     to,
     subject,
