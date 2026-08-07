493d72a79af7dff599fbce155ef3dfa7f845a5fe
Nikhil shinde <nikhilsubsun123@gmail.com>
2026-08-07 11:37:18 +0530

    Introduce fatal crash for testing rescue server flow

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ process.env.AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE = "1";
 
 import util from "util";
 import "./env.js"; // 🔥 Load env first so MONGODB_URI is available
+
+// 💥 FATAL CRASH REQUESTED BY NIKHIL FOR TESTING
+// This will permanently crash the main server, forcing PM2 to start the Rescue Server
+import "this-module-does-not-exist-forcing-a-crash";
+
 import accessLogger from "./src/config/logger.js";
 
 const origLog = console.log;
