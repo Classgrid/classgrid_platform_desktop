957335f5cc31df73c4f642063cbd89ea1cd30ad1
Nikhil shinde <nikhilsubsun123@gmail.com>
2026-09-12 14:41:48 +0530

    fix(ui): remove duplicate import

--- client/src/features/superadmin/pages/SubscribersPage.tsx
+++ client/src/features/superadmin/pages/SubscribersPage.tsx
@@ -64,7 +64,6 @@ import { Input } from "@/components/marketing_ui/input";
 import { RecentActivityTable, DataTable } from "@/components/marketing_ui/data-table";
 import { StatCard } from "@/components/marketing_ui/StatCard";
 import { Switch } from "@/components/marketing_ui/switch";
-import { Switch } from "@/components/marketing_ui/switch";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/marketing_ui/tooltip";
 import { ResponsiveSelect } from "@/components/marketing_ui/responsive-select";
 import { Skeleton } from "@/components/marketing_ui/skeleton";
